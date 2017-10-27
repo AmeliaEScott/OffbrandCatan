a87b9a22a75a2bb47a7a15fa630ddcd4378fea63
Timmy <timjimski@gmail.com>
2017-10-27 14:52:02 -0400

    Made sign in page EVEN MORE less terrible

--- login.py
+++ login.py
@@ -43,54 +43,57 @@ def register():
     return render_template('signin.html', register=True, password_min_length=PASSWORD_MIN_LENGTH)
 
 
-@login_page.route('/register', methods=['POST'])
 @login_page.route('/login', methods=['POST'])
 def dologin():
     username = request.form['username']
     password = request.form['password']
-    login = 'login' in request.form
-    register = 'register' in request.form
-    if login:
-        current_app.logger.debug("Attempting login with username %s", username)
-        user = db_session.query(User).filter(User.username == username).first()
-        if user is None:  # If the username is not found
-            current_app.logger.debug("User %s does not exist.", username)
-            # Doing this to prevent timing attacks or whatever.
-            # Idk, I don't actually know anything about security, I've just heard of timing attacks before.
-            bcrypt.hashpw(b"pw", bcrypt.gensalt(PASSWORD_SALT_ROUNDS))
-            return render_template('signin.html', error_login=True, username=username,
-                                   password_min_length=PASSWORD_MIN_LENGTH)
-        elif bcrypt.checkpw(password.encode(), user.passwordhash):  # The password is correct
-            current_app.logger.debug("Password is correct!")
-            return processlogin(user)
-        else:  # Username exists, wrong password
-            # TODO: Count password attempts
-            current_app.logger.debug("Password was incorrect.")
-            return render_template('signin.html', error_login=True, username=username,
-                                   password_min_length=PASSWORD_MIN_LENGTH)
-    elif register:
-        error_username_invalid = len(username) > 100 or not re.match("^[a-zA-Z0-9_]+$", username)
-        error_password_length = len(password) < PASSWORD_MIN_LENGTH or len(password) > 256
 
-        current_app.logger.debug("Invalid username: %s, invalid password: %s",
-                                 error_username_invalid, error_password_length)
+    current_app.logger.debug("Attempting login with username %s", username)
+    user = db_session.query(User).filter(User.username == username).first()
+    if user is None:  # If the username is not found
+        current_app.logger.debug("User %s does not exist.", username)
+        # Doing this to prevent timing attacks or whatever.
+        # Idk, I don't actually know anything about security, I've just heard of timing attacks before.
+        bcrypt.hashpw(b"pw", bcrypt.gensalt(PASSWORD_SALT_ROUNDS))
+        return render_template('signin.html', error_login=True, username=username,
+                               password_min_length=PASSWORD_MIN_LENGTH)
+    elif bcrypt.checkpw(password.encode(), user.passwordhash):  # The password is correct
+        current_app.logger.debug("Password is correct!")
+        return processlogin(user)
+    else:  # Username exists, wrong password
+        # TODO: Count password attempts
+        current_app.logger.debug("Password was incorrect.")
+        return render_template('signin.html', error_login=True, username=username,
+                               password_min_length=PASSWORD_MIN_LENGTH)
 
-        if error_password_length or error_username_invalid:
-            return render_template('signin.html', register=True, error_username_invalid=error_username_invalid,
-                                   error_password_length=error_password_length, username=username,
-                                   password_min_length=PASSWORD_MIN_LENGTH)
-        existinguser = db_session.query(User).filter(User.username == username).first()
-        if existinguser is not None:  # This username is already taken
-            current_app.logger.debug("Username is already taken")
-            return render_template('signin.html', register=True, error_username_taken=True, username=username,
-                                   password_min_length=PASSWORD_MIN_LENGTH)
 
-        passwordhash = bcrypt.hashpw(password.encode(), bcrypt.gensalt(PASSWORD_SALT_ROUNDS))
-        current_app.logger.debug("Password hash: %s", passwordhash)
-        newuser = User(username, passwordhash)
-        db_session.add(newuser)
-        db_session.commit()
-        return processlogin(newuser)
+@login_page.route('/register', methods=['POST'])
+def doregister():
+    username = request.form['username']
+    password = request.form['password']
+
+    error_username_invalid = len(username) > 100 or not re.match("^[a-zA-Z0-9_]+$", username)
+    error_password_length = len(password) < PASSWORD_MIN_LENGTH or len(password) > 256
+
+    current_app.logger.debug("Invalid username: %s, invalid password: %s",
+                             error_username_invalid, error_password_length)
+
+    if error_password_length or error_username_invalid:
+        return render_template('signin.html', register=True, error_username_invalid=error_username_invalid,
+                               error_password_length=error_password_length, username=username,
+                               password_min_length=PASSWORD_MIN_LENGTH)
+    existinguser = db_session.query(User).filter(User.username == username).first()
+    if existinguser is not None:  # This username is already taken
+        current_app.logger.debug("Username is already taken")
+        return render_template('signin.html', register=True, error_username_taken=True, username=username,
+                               password_min_length=PASSWORD_MIN_LENGTH)
+
+    passwordhash = bcrypt.hashpw(password.encode(), bcrypt.gensalt(PASSWORD_SALT_ROUNDS))
+    current_app.logger.debug("Password hash: %s", passwordhash)
+    newuser = User(username, passwordhash)
+    db_session.add(newuser)
+    db_session.commit()
+    return processlogin(newuser)
 
 
 def processlogin(user):
