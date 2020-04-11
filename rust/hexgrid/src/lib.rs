5917997a09578e11a86c779a3f1e2f50cf923c98
Timmy <timjimski@gmail.com>
2020-04-11 18:44:24 +0200

    Added hex grid struct

--- rust/hexgrid/src/lib.rs
+++ rust/hexgrid/src/lib.rs
@@ -1,2 +1,44 @@
 pub mod hex_coordinates;
+//pub mod demo;
 
+use std::collections::HashMap;
+use serde::{Serialize, Deserialize};
+use hex_coordinates::{HexCoord, Tile, Edge, Corner};
+
+#[derive(Debug, Eq, PartialEq, Serialize, Deserialize)]
+pub struct HexGrid<T, E, C> {
+    pub tiles: HashMap<Tile, T>,
+    pub edges: HashMap<Edge, E>,
+    pub corners: HashMap<Corner, C>
+}
+
+impl<T, E, C> HexGrid<T, E, C> {
+    pub fn new() -> Self {
+        HexGrid {
+            tiles: HashMap::new(),
+            edges: HashMap::new(),
+            corners: HashMap::new()
+        }
+    }
+
+    pub fn get_tile_neighbors<CoordType: HexCoord>(&self, coords: &CoordType) -> Vec<&T> {
+        coords.get_tile_neighbors().into_iter()
+            .map(|c| self.tiles.get(&c))
+            .flatten()
+            .collect()
+    }
+
+    pub fn get_corner_neighbors<CoordType: HexCoord>(&self, coords: &CoordType) -> Vec<&C> {
+        coords.get_corner_neighbors().into_iter()
+            .map(|c| self.corners.get(&c))
+            .flatten()
+            .collect()
+    }
+
+    pub fn get_edge_neighbors<CoordType: HexCoord>(&self, coords: &CoordType) -> Vec<&E> {
+        coords.get_edge_neighbors().into_iter()
+            .map(|c| self.edges.get(&c))
+            .flatten()
+            .collect()
+    }
+}
---
+++ rust/hexgrid/tests/hexgrid_test.rs
@@ -0,0 +1,80 @@
+#[cfg(test)]
+mod hexgrid_tests {
+    use hexgrid::HexGrid;
+    use hexgrid::hex_coordinates::{Tile, Corner, CornerDirection, Edge, EdgeDirection};
+
+    #[test]
+    pub fn tile_neighbor_test() {
+        let mut grid: HexGrid<i32, &str, bool> = HexGrid::new();
+
+        let tile1 = Tile::new(0, 0);
+        let tile2 = Tile::new(1, 0);
+        let tile3 = Tile::new(-1, 0);
+
+        grid.tiles.insert(tile1, 1);
+        assert_eq!(grid.get_tile_neighbors(&tile1).len(), 0);
+
+        grid.tiles.insert(tile2, 2);
+        assert_eq!(grid.get_tile_neighbors(&tile1).len(), 1);
+
+        grid.tiles.insert(tile3, 3);
+        assert_eq!(grid.get_tile_neighbors(&tile1).len(), 2);
+    }
+
+    #[test]
+    pub fn edge_neighbor_test() {
+        let mut grid: HexGrid<i32, &str, bool> = HexGrid::new();
+
+        let edge1 = Edge::new(0, 0, EdgeDirection::East);
+        let edge2 = Edge::new(0, 0, EdgeDirection::Northeast);
+        let edge3 = Edge::new(0, 0, EdgeDirection::Southeast);
+
+        grid.edges.insert(edge1, "1");
+        assert_eq!(grid.get_edge_neighbors(&edge1).len(), 0);
+
+        grid.edges.insert(edge2, "2");
+        assert_eq!(grid.get_edge_neighbors(&edge1).len(), 1);
+
+        grid.edges.insert(edge3, "3");
+        assert_eq!(grid.get_edge_neighbors(&edge1).len(), 2);
+    }
+
+    #[test]
+    pub fn corner_neighbor_test() {
+        let mut grid: HexGrid<i32, &str, bool> = HexGrid::new();
+
+        let corner1 = Corner::new(0, 0, CornerDirection::North);
+        let corner2 = Corner::new(0, 0, CornerDirection::Northeast);
+        let corner3 = Corner::new(0, 0, CornerDirection::Northwest);
+
+        grid.corners.insert(corner1, true);
+        assert_eq!(grid.get_corner_neighbors(&corner1).len(), 0);
+
+        grid.corners.insert(corner2, false);
+        assert_eq!(grid.get_corner_neighbors(&corner1).len(), 1);
+
+        grid.corners.insert(corner3, false);
+        assert_eq!(grid.get_corner_neighbors(&corner1).len(), 2);
+    }
+
+    #[test]
+    pub fn serde_test() {
+        let mut grid: HexGrid<i32, &str, bool> = HexGrid::new();
+        grid.tiles.insert(Tile::new(0, 0), 0);
+        grid.tiles.insert(Tile::new(1, 0), 1);
+        grid.tiles.insert(Tile::new(-1, 0), 2);
+
+        grid.edges.insert(Edge::new(0, 0, EdgeDirection::East), "0");
+        grid.edges.insert(Edge::new(0, 0, EdgeDirection::Northeast), "1");
+        grid.edges.insert(Edge::new(0, 0, EdgeDirection::Southeast), "2");
+
+        grid.corners.insert(Corner::new(0, 0, CornerDirection::North), false);
+        grid.corners.insert(Corner::new(0, 0, CornerDirection::Northeast), true);
+        grid.corners.insert(Corner::new(0, 0, CornerDirection::Northwest), true);
+
+        let s = serde_json::to_string(&grid).unwrap();
+        println!("{}", s);
+        let grid2: HexGrid<i32, &str, bool> = serde_json::from_str(&s).unwrap();
+        assert_eq!(grid, grid2);
+    }
+}
